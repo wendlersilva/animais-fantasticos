d5594953e12d654462dc6d82deef195d0fd16dad
Wendler <wendlerconstancio@gmail.com>
2024-03-01 12:15:01 -0300

    Accordion List

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+//Navegacao Scroll
+
+function initTabNav () {
+
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 
@@ -17,3 +21,27 @@ tabMenu.forEach((itemMenu, index) => {
         activeTab(index);
     });
 });
+}
+initTabNav();
+
+//Lista Faq
+
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'ativo';
+    
+    if(accordionList.length) {
+      accordionList[0].classList.add(activeClass);
+      accordionList[0].nextElementSibling.classList.add(activeClass);
+  
+      function activeAccordion() {
+        this.classList.toggle(activeClass);
+        this.nextElementSibling.classList.toggle(activeClass);
+      }
+  
+      accordionList.forEach((item) => {
+        item.addEventListener('click', activeAccordion);
+      });
+    }
+  }
+  initAccordion();
